7c86e3adb80e1deaa8ed57ae5b4e6d4b20e11385
Tino Wagner <ich@tinowagner.com>
2017-02-16 00:09:13 +0100

    Set version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gwyfile',
-    version='0.1',
+    version='0.2.0',
     description='Pythonic implementation of the Gwyddion file format',
     author='Tino Wagner',
     author_email='ich@tinowagner.com',
