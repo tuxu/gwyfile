f34c4815aee0ffcd49b9f247454e5b514eca0022
Tino Wagner <ich@tinowagner.com>
2017-02-16 00:07:39 +0100

    Cleanup

--- gwyfile/util.py
+++ gwyfile/util.py
@@ -1,16 +1,16 @@
 def find_datafields(obj):
-    """ Return pairs of (``number``, ``title``) for all available
-        data fields in ``obj``.
+    """Return pairs of (`number`, `title`) for all available data fields in
+       `obj`.
     """
     token = '/data/title'
-    channels = [int(k[1:-len(token)]) for k, v in obj.items()
+    channels = [int(k[1:-len(token)]) for k, _ in obj.items()
                 if k.endswith(token)]
     titles = [obj['/{}/data/title'.format(ch)] for ch in channels]
     return zip(channels, titles)
 
 
 def get_datafields(obj):
-    """ Return a dictionary of titles and their corresponding data fields.
+    """Return a dictionary of titles and their corresponding data fields.
     """
     return {
         v: obj['/{chnum}/data'.format(chnum=k)]
